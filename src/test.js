47b8c1ac417bdec7c0d510687ab9ff5a6e95aa4b
Xihao <xihao95@gmail.com>
2020-01-07 11:55:11 +0800

    Minor updates

--- src/test.js
+++ src/test.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function Test() {
   return (
-    <div className="Test">
-      <header className="Test-header">
-        <img src={logo} className="Test-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Test-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Test</h1>
   );
 }
 
